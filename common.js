1fd81cd5f75e93bcb37698ad95628d14175c9ac9
Kristina Yantser <kristy.net.ua@mail.ru>
2019-12-12 19:50:32 +0300

    Я попыталась сделать задание в 01 Знакомство

--- common.js
+++ common.js
@@ -1,4 +1,25 @@
 
-window.addEventListener("DOMContentLoaded", () => {
+var fireballSize = 22;
+
+var getFireballSpeed = function(left) {
+    if (left == true) {
+        return 5;
+    } else {
+        return 2;
+    }
+};
+
+var wizardSpeed = 3;
+var wizardWidth = 70;
+
+var getWizardHeight = function() {
+    return 1.337 * wizardWidth;
+};
+
+var getWizardX = function(width) {
+
+};
+
+var getWizardY = function () {
     
-});
+};
