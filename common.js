01a33807aef7f371a4c60f6c88ff0aaf26c2f97e
Kristina Yantser <kristy.net.ua@mail.ru>
2019-12-18 14:28:25 +0300

    Дополнительные методы в canvas

--- common.js
+++ common.js
@@ -3,5 +3,18 @@ var canvas = document.getElementById('canvas');
 
 var ctx = canvas.getContext('2d');
 
-ctx.fillStyle = 'blue'; //сюда можно писать rgba, цвет, #ffffff
+var gradient = ctx.createRadialGradient(150,72,0,150,72,72);
+gradient.addColorStop(0, 'green');
+gradient.addColorStop(1, 'rgba(0, 255, 0, 0)');
+
+
+ctx.fillStyle = gradient;
+// ctx.fillStyle = 'blue'; //сюда можно писать rgba, цвет, #ffffff
 ctx.fillRect(0,0,300,150); // 0, 0 - левый верхний угол, 300 - ширина, 150 - высота
+
+ctx.strokeStyle = 'blue';
+ctx.strokeRect(75, 0, 150, 150); // обвести объект
+
+ctx.clearRect(0,0,300,150); // очистить объект
+
+
