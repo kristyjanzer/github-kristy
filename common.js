a5358ada76f54e97cc7ced5cf51941d739ae8435
Kristina Yantser <kristy.net.ua@mail.ru>
2019-12-18 12:22:01 +0300

    В раковине ? посуды

--- common.js
+++ common.js
@@ -1,53 +1,8 @@
-// Создаем массив со словами
-var words = [
-    "яблоко",
-    "арбуз",
-    "программа",
-    "оладушек"
-  ];
-  
-  // Выбираем случайное слово
-  var word = words[Math.floor(Math.random() * words.length)];
-  
-  // Создаем итоговый массив
-  var answerArray = [];
-  for (var i = 0; i < word.length; i++) {
-    answerArray[i] = "_";
+var washItems = function(itemsLeft) {
+  while (itemsLeft) {
+    itemsLeft--;
+    console.log("В раковине " + itemsLeft + " посуды");
   }
-  
-  var remainingLetters = word.length;
-  var guesses = 20;
-  
-  // Игровой цикл
-  while (remainingLetters > 0 && guesses > 0) {
-    // Показываем состояние игры
-    alert(answerArray.join(" "));
-    
-    // Запрашиваем вариант ответа
-    var guess = prompt("Угадайте букву, или нажмите Отмена для выхода из игры.");
-    if (guess === null) {
-      // Выходим из игрового цикла
-      break;
-    } else if (guess.length !== 1) {
-      alert("Пожалуйста, введите одиночную букву.");
-    } else {
-      guesses--;
-      guess = guess.toLowerCase();
-      // Обновляем состояние игры
-      for (var j = 0; j < word.length; j++) {
-        if (word[j] === guess && answerArray[j] === "_") {
-          answerArray[j] = guess;
-          remainingLetters--;
-        }
-      }
-    }
-  // Конец игрового цикла
-  }
-  
-  // Отображаем ответ и поздравляем игрока
-  alert(answerArray.join(" "));
-  if (guesses > 0) {
-    alert("Отлично! Было загадано слово " + word);
-  } else {
-    alert("Очень плохо! Было загадано слово " + word);
-  }
+};
+
+washItems(10);
