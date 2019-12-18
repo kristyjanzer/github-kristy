3ad8f9ae438d22960a620b9b1f61ccd9b36a68db
Kristina Yantser <kristy.net.ua@mail.ru>
2019-12-18 15:23:32 +0300

    Работа с текстом в canvas

--- common.js
+++ common.js
@@ -3,15 +3,7 @@ var canvas = document.getElementById('canvas');
 
 var ctx = canvas.getContext('2d');
 
-ctx.beginPath(); // начало контура
-ctx.moveTo(100,100);
-ctx.lineTo(80,60);
-ctx.lineTo(110,80);
-ctx.lineTo(125,40);
-ctx.lineTo(140,80);
-ctx.lineTo(170,60);
-ctx.lineTo(150,100);
-ctx.lineTo(150,100);
-ctx.bezierCurveTo(140,90,110,90,100,100);
-ctx.closePath(); // конец контура
-ctx.stroke(); // отрисовка контура
+ctx.font = '30px Tahoma';
+ctx.textBaseline = 'hanging';
+ctx.fillText('Привет', 0, 10);
+
