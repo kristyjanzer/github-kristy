fb7e66fe2d0f507e4b64e372f06dad27283dd3e7
Kristina Yantser <kristy.net.ua@mail.ru>
2019-12-18 14:25:29 +0300

    Обычный цвет в canvas

--- common.js
+++ common.js
@@ -3,13 +3,5 @@ var canvas = document.getElementById('canvas');
 
 var ctx = canvas.getContext('2d');
 
-var gradient = ctx.createRadialGradient(150,72,0,150,72,72);
-gradient.addColorStop(0, 'green');
-gradient.addColorStop(1, 'rgba(0, 255, 0, 0)');
-
-
-ctx.fillStyle = gradient;
-// ctx.fillStyle = 'blue'; //сюда можно писать rgba, цвет, #ffffff
+ctx.fillStyle = 'blue'; //сюда можно писать rgba, цвет, #ffffff
 ctx.fillRect(0,0,300,150); // 0, 0 - левый верхний угол, 300 - ширина, 150 - высота
-
-
