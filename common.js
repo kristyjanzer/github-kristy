bef4a0456c6934a5229ea8b4666834d0f47ce68f
Kristina Yantser <kristy.net.ua@mail.ru>
2019-12-18 14:36:24 +0300

    Нарисовали корону в canvas

--- common.js
+++ common.js
@@ -3,18 +3,15 @@ var canvas = document.getElementById('canvas');
 
 var ctx = canvas.getContext('2d');
 
-var gradient = ctx.createRadialGradient(150,72,0,150,72,72);
-gradient.addColorStop(0, 'green');
-gradient.addColorStop(1, 'rgba(0, 255, 0, 0)');
-
-
-ctx.fillStyle = gradient;
-// ctx.fillStyle = 'blue'; //сюда можно писать rgba, цвет, #ffffff
-ctx.fillRect(0,0,300,150); // 0, 0 - левый верхний угол, 300 - ширина, 150 - высота
-
-ctx.strokeStyle = 'blue';
-ctx.strokeRect(75, 0, 150, 150); // обвести объект
-
-ctx.clearRect(0,0,300,150); // очистить объект
-
-
+ctx.beginPath(); // начало контура
+ctx.moveTo(100,100);
+ctx.lineTo(80,60);
+ctx.lineTo(110,80);
+ctx.lineTo(125,40);
+ctx.lineTo(140,80);
+ctx.lineTo(170,60);
+ctx.lineTo(150,100);
+ctx.lineTo(150,100);
+ctx.bezierCurveTo(140,90,110,90,100,100);
+ctx.closePath(); // конец контура
+ctx.stroke(); // отрисовка контура
