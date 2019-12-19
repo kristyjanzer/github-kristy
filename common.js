7a9b70294655e19a174ffbd7d7b8d75fcc2cbc2a
Kristina Yantser <kristy.net.ua@mail.ru>
2019-12-19 14:11:03 +0300

    Делаю canvas по примерам developer.mozilla

--- common.js
+++ common.js
@@ -1,9 +1,22 @@
+var players = [
+  {
+    name: 'Kate',
+    time: 125
+  },
+  {
+    name: 'Sasha',
+    time: 105
+  },
+  {
+    name: 'Rita',
+    time: 93
+  }
+];
 
-var canvas = document.getElementById('canvas');
-
-var ctx = canvas.getContext('2d');
-
-ctx.font = '30px Tahoma';
-ctx.textBaseline = 'hanging';
-ctx.fillText('Привет', 0, 10);
-
+for (var i=0; i < players.lenght; i++) {
+  if (players.time > 100) {
+    console.log(players[i]);
+  } else {
+    console.log("Не найдено");
+  }
+}
