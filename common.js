087a2dac0508f7a15e5bddc9491bd8bebf7f37a9
Kristina Yantser <kristy.net.ua@mail.ru>
2020-01-07 14:36:40 +0300

    отрабатываю onclick в html

--- common.js
+++ common.js
@@ -1,11 +1,3 @@
-var setupOpen = document.querySelector('.setup-open');
-var setupClose = document.querySelector('.setup-close');
-var btn = document.querySelector('button');
-
-btn.addEventListener('click', function() {
-    setupOpen.classList.remove('hidden');
-});
-
-setupClose.addEventListener('click', function() {
-    setupOpen.classList.add('hidden');
-});
+function run() {
+    alert("Here I am");
+}
