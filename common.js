c921aa37017811b4cd6118a008573b9bdc4f795e
Kristina Yantser <kristy.net.ua@mail.ru>
2020-01-17 14:59:37 +0300

    Как было до модальных кнопок

--- common.js
+++ common.js
@@ -1,20 +1,23 @@
 var buttons = document.querySelectorAll('button');
-var retext = document.createElement('div');
-    retext.classList.add('retext'); 
 
 var reClick = function(butt) {
   butt.addEventListener('click', function(evt) {
-        butt.append(retext);
-        retext.textContent = butt.value;
+    (function() {
+      var retext = document.createElement('div');
+      retext.classList.add('retext'); 
+      butt.append(retext);
+      retext.textContent = butt.value;
+    })();
   });
 };
 
 var teClick = function(butt) {
-  butt.addEventListener('click', function(evt) {
+  butt.removeEventListener('click', function(evt) {
+    if(evt.type === click) {
       retext.classList.remove('retext'); 
+    }
   });
 };
-
 
 for (var i = 0; i < buttons.length; i++) {
   var butt = buttons[i];
